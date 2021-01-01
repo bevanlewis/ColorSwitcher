039166ddcaece5d44f5bff5978285d4fb88cb1b4
bevanlewis <bevanlewis2510@gmail.com>
2021-01-01 16:20:16 +0400

    Added animation function to the button to change the background once.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,26 +7,39 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
+  // Initializing variables
   AnimationController controller;
   Animation animation;
+  Color startColor = Colors.green;
+  Color endColor = Colors.blue;
 
   @override
+  // Animation gets run when the app is launched
   void initState() {
     super.initState();
 
     // Controller
     controller = AnimationController(
-      duration: Duration(seconds: 1),
+      duration: Duration(seconds: 5),
       vsync: this,
     );
 
-    // Animation
+    // Animation - Transition of one color to the other
     animation = ColorTween(
-      begin: Colors.blueGrey,
-      end: Colors.white,
+      begin: startColor,
+      end: endColor,
     ).animate(controller);
 
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else {
+    //     controller.forward();
+    //   }
+    // });
+
     // Helps rebuild the app with changing background and prints the changed values to console
+    // Listener is called every time the value of the animation changes
     controller.addListener(() {
       setState(() {});
       print(animation.value);
@@ -37,6 +50,7 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // backgroundColor changes based on the values of animation which are also colors
         backgroundColor: animation.value,
         body: SafeArea(
           child: Center(
@@ -48,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage>
                   style: TextStyle(color: Colors.black),
                 ),
                 onTap: () {
-                  controller.forward();
+                  controller.forward(from: 0.0);
                   print('This actually works');
                 },
               ),
