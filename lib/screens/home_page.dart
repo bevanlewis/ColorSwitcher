0367457aed001e7b16be907c7a4c9ec97b529139
bevanlewis <bevanlewis2510@gmail.com>
2021-01-01 15:58:07 +0400

    Added animation function to the button to change the background once.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,12 +5,39 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Controller
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    // Animation
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(controller);
+
+    // Helps rebuild the app with changing background and prints the changed values to console
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: animation.value,
         body: SafeArea(
           child: Center(
             child: Container(
@@ -21,6 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   style: TextStyle(color: Colors.black),
                 ),
                 onTap: () {
+                  controller.forward();
                   print('This actually works');
                 },
               ),
